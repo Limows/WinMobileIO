4c60ab7edd689af278b4cc9f8289e6d3bd3e58bf
Limows <Limows2013@yandex.ru>
2021-01-18 13:26:44 +0300

    Fix for AssemblyBuildDate

--- WinMobileNetCFExt/AboutBox.cs
+++ WinMobileNetCFExt/AboutBox.cs
@@ -23,6 +23,23 @@ namespace WinMobileNetCFExt.About
             this.textBoxDescription.Text = AssemblyDescription;
         }
 
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            }
+        }
+
         public string AssemblyVersion
         {
             get
@@ -35,7 +52,32 @@ namespace WinMobileNetCFExt.About
         {
             get
             {
-                return File.GetLastWriteTime(Assembly.GetExecutingAssembly().GetName().CodeBase).ToString("dd.MM.yy");
+                //return File.GetCreationTime(Assembly.GetExecutingAssembly().GetName().CodeBase).ToString("dd.MM.yy");
+                string filePath = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+                const int c_PeHeaderOffset = 60;
+                const int c_LinkerTimestampOffset = 8;
+                byte[] b = new byte[2048];
+                System.IO.Stream s = null;
+
+                try
+                {
+                    s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    s.Read(b, 0, 2048);
+                }
+                finally
+                {
+                    if (s != null)
+                    {
+                        s.Close();
+                    }
+                }
+
+                int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+                int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+                dt = dt.AddSeconds(secondsSince1970);
+                dt = dt.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(dt).Hours);
+                return dt.ToString("dd.MM.yy");
             }
         }
 
